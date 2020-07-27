9195014caf0bf015c2c48919a36471da0a668ebe
yuriybk <email2yurii@gmail.com>
2020-07-27 12:27:18 +0300

    minor

--- node/creational/factory/index.js
+++ node/creational/factory/index.js
@@ -3,7 +3,7 @@ const userFactory = require('./userFactory');
 const user1 = userFactory('John Doe', 100);
 const user2 = userFactory('John Smith', 100, 'employee', 'John and co');
 
-user1.payDay(100);
+user2.payDay(101);
 
 console.log( user1.toString() );
 console.log( user2.toString() );
